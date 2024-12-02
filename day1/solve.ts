9253b3b9732984f82d10ae61b7f2946611e044d3
Luke Storry <luke.storry@faculty.ai>
2024-12-02 16:42:50 +0000

    tidy

--- day1/solve.ts
+++ day1/solve.ts
@@ -1,34 +1,27 @@
 import { solve } from "../runner/typescript";
-import { sum, countBy } from "lodash";
+import { sum, countBy, zip } from "lodash";
 
-type Parsed = [number[], number[]];
-function parser(input: string): Parsed {
-  const [left, right] = [[], []];
-  input.split("\n").forEach((line) => {
-    const [a, b] = line.split("   ").map(Number);
-    left.push(a);
-    right.push(b);
-  });
-  return [left, right];
+function parser(input: string): number[][] {
+  const numbers = input
+    .split("\n")
+    .map((line) => line.match(/\d+/g).map(Number));
+  return zip(...numbers);
 }
 
-function part1([left, right]: Parsed): number {
-  const ls = left.sort();
-  const rs = right.sort();
-  // sum of differences between each pair
-  return sum(ls.map((l, i) => Math.abs(l - rs[i])));
+function part1([left, right]: number[][]): number {
+  const sortedRight = right.toSorted();
+  return sum(left.toSorted().map((l, i) => Math.abs(l - sortedRight[i])));
 }
 
-function part2([left, right]: Parsed): number {
+function part2([left, right]: number[][]): number {
   const counts = countBy(right);
   return sum(left.map((l) => l * counts[l] || 0));
 }
 
 solve({
-  parser: parser,
-  part1: part1,
-  part2: part2,
-
+  parser,
+  part1,
+  part2,
   part1Tests: [["3   4\n4   3\n2   5\n1   3\n3   9\n3   3", 11]],
   part2Tests: [["3   4\n4   3\n2   5\n1   3\n3   9\n3   3", 31]],
 });

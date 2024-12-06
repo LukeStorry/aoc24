2f2dd0386cdf57a7727e8969a3fb58fbc465bfc3
Luke Storry <luke.storry@faculty.ai>
2024-12-06 09:50:28 +0000

    optimisations

--- day6/solve.ts
+++ day6/solve.ts
@@ -1,6 +1,14 @@
 import { solve } from "../runner/typescript";
 import { max, sum, groupBy, values, uniqBy, isEqual, omit } from "lodash";
 
+const directions = [
+  [0, -1],
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+];
+
+type Parsed = ReturnType<typeof parser>;
 function parser(input: string) {
   const lines = input.split("\n");
   const size = lines.length;
@@ -15,14 +23,6 @@ function parser(input: string) {
   const emptyCells = cells.filter(({ char }) => char === ".");
   return { obstacles, start, size, emptyCells };
 }
-type Parsed = ReturnType<typeof parser>;
-
-const directions = [
-  { dx: 0, dy: -1 },
-  { dx: 1, dy: 0 },
-  { dx: 0, dy: 1 },
-  { dx: -1, dy: 0 },
-];
 
 function tryPatrol({ obstacles, start, size }: Parsed): {
   cyclic: boolean;
@@ -30,30 +30,25 @@ function tryPatrol({ obstacles, start, size }: Parsed): {
 } {
   let visited = [];
   const visitedSet = new Set();
-  let current = start;
+  let { x, y, direction } = start;
   while (true) {
-    if (
-      current.x >= size ||
-      current.y >= size ||
-      current.x < 0 ||
-      current.y < 0
-    )
+    if (x >= size || y >= size || x < 0 || y < 0)
       return { cyclic: false, visited };
 
-    // Set check instead of array.some gets from 8s to 0.1s!
-    const hash = `${current.x},${current.y},${current.direction}`;
+    const hash = `${x},${y},${direction}`;
     if (visitedSet.has(hash)) return { cyclic: true, visited };
-    visited.push(current);
+    visited.push({ x, y });
     visitedSet.add(hash);
 
-    const next = {
-      ...current,
-      x: current.x + directions[current.direction].dx,
-      y: current.y + directions[current.direction].dy,
-    };
-    current = obstacles.has(`${next.x},${next.y}`)
-      ? { ...current, direction: (current.direction + 1) % 4 }
-      : next;
+    const [dx, dy] = directions[direction];
+    const nextX = x + dx;
+    const nextY = y + dy;
+    if (obstacles.has(`${nextX},${nextY}`)) {
+      direction = (direction + 1) % 4;
+    } else {
+      x = nextX;
+      y = nextY;
+    }
   }
 }
 

3fc268f284d81255d373c37e8362e8b4b882456c
Luke Storry <luke.storry@faculty.ai>
2024-12-07 17:18:17 +0000

    more clean

--- day7/python.py
+++ day7/python.py
@@ -1,30 +1,27 @@
-input = open("day7/input.txt").read()
-
-
 equations = [
   (
     int(line.split(": ")[0]),
     [int(x) for x in line.split(": ")[1].split(" ")],
   )
-  for line in input.splitlines()
+  for line in open("day7/input.txt").read().splitlines()
 ]
 
 
 def can_be_solved(target: int, values: list[int], with_concat=False):
   queue = [(1, values[0])]
-
   while queue:
     index, subtotal = queue.pop()
     if index == len(values):
-      if subtotal == target:
-        return True
-      continue
+      if subtotal != target:
+        continue
+      return True
 
     next_val = values[index]
     queue.append((index + 1, subtotal + next_val))
     queue.append((index + 1, subtotal * next_val))
     if with_concat:
       queue.append((index + 1, int(str(subtotal) + str(next_val))))
+
   return False
 
 
@@ -33,12 +30,9 @@ part1 = sum(
   for target, values in equations
   if can_be_solved(target, values)
 )
-
-
 part2 = sum(
   target
   for target, values in equations
   if can_be_solved(target, values, with_concat=True)
 )
-
 print(part1, part2)

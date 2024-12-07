70977f7cb61d6ec3f26a044cd8caf97d3761e0c4
Luke Storry <luke.storry@faculty.ai>
2024-12-07 13:00:21 +0000

    7p1

---
+++ day7/solve.ts
@@ -0,0 +1,52 @@
+import { solve } from "../runner/typescript";
+import { max, sum, groupBy } from "lodash";
+
+type Equation = [number, number[]];
+
+function parser(input: string) {
+  return input.split("\n").map<Equation>((line) => {
+    const [targetStr, valsStr] = line.split(": ");
+    return [Number(targetStr), valsStr.split(" ").map(Number)];
+  });
+}
+
+function canBeSolved(
+  target: number,
+  values: number[],
+  subtotal = null
+): boolean {
+  if (!subtotal) subtotal = values.shift();
+  if (!values.length) return subtotal == target;
+  const [next, ...rest] = values;
+  return (
+    canBeSolved(target, rest, subtotal + next) ||
+    canBeSolved(target, rest, subtotal * next)
+  );
+}
+
+function part1(equations: Equation[]): number {
+  return sum(
+    equations.filter((e) => canBeSolved(...e)).map(([target, _]) => target)
+  );
+}
+
+function part2(values: Equation[]): number {
+  return 0;
+}
+
+solve({
+  parser,
+  part1,
+  // part2,
+
+  part1Tests: [
+    [
+      "190: 10 19\n3267: 81 40 27\n83: 17 5\n156: 15 6\n7290: 6 8 6 15\n161011: 16 10 13\n192: 17 8 14\n21037: 9 7 18 13\n292: 11 6 16 20",
+      3749,
+    ],
+  ],
+  part2Tests: [
+    // ["aaa", 0],
+    // ["a", 0],
+  ],
+});

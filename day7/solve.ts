377b03478164867801c354a4f47c864f9eef61e4
Luke Storry <luke.storry@faculty.ai>
2024-12-07 13:19:02 +0000

    part 2

--- day7/solve.ts
+++ day7/solve.ts
@@ -1,5 +1,5 @@
 import { solve } from "../runner/typescript";
-import { max, sum, groupBy } from "lodash";
+import { sum } from "lodash";
 
 type Equation = [number, number[]];
 
@@ -10,34 +10,37 @@ function parser(input: string) {
   });
 }
 
-function canBeSolved(
-  target: number,
-  values: number[],
-  subtotal = null
-): boolean {
-  if (!subtotal) subtotal = values.shift();
-  if (!values.length) return subtotal == target;
-  const [next, ...rest] = values;
-  return (
-    canBeSolved(target, rest, subtotal + next) ||
-    canBeSolved(target, rest, subtotal * next)
-  );
+function canBeSolved([target, values]: Equation, withConcat = false): boolean {
+  function solve(remaining: number[], subtotal: number) {
+    if (!remaining.length) return subtotal == target;
+    const [next, ...rest] = remaining;
+    return (
+      solve(rest, subtotal + next) ||
+      solve(rest, subtotal * next) ||
+      (withConcat && solve(rest, Number(`${subtotal}${next}`)))
+    );
+  }
+
+  const subtotal = values.shift();
+  return solve(values, subtotal);
 }
 
 function part1(equations: Equation[]): number {
   return sum(
-    equations.filter((e) => canBeSolved(...e)).map(([target, _]) => target)
+    equations.filter((e) => canBeSolved(e)).map(([target, _]) => target)
   );
 }
 
-function part2(values: Equation[]): number {
-  return 0;
+function part2(equations: Equation[]): number {
+  return sum(
+    equations.filter((e) => canBeSolved(e, true)).map(([target, _]) => target)
+  );
 }
 
 solve({
   parser,
   part1,
-  // part2,
+  part2,
 
   part1Tests: [
     [
@@ -46,7 +49,9 @@ solve({
     ],
   ],
   part2Tests: [
-    // ["aaa", 0],
-    // ["a", 0],
+    [
+      "190: 10 19\n3267: 81 40 27\n83: 17 5\n156: 15 6\n7290: 6 8 6 15\n161011: 16 10 13\n192: 17 8 14\n21037: 9 7 18 13\n292: 11 6 16 20",
+      11387,
+    ],
   ],
 });

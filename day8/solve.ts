27c3337f7953d6ed30025eefdfef28b5901b2ac2
Luke Storry <luke.storry@faculty.ai>
2024-12-08 13:21:01 +0000

    cleanup

--- day8/solve.ts
+++ day8/solve.ts
@@ -1,72 +1,53 @@
 import { solve } from "../runner/typescript";
-import { groupBy } from "lodash";
+import { groupBy, range } from "lodash";
 
 type Point = { x: number; y: number };
-type Parsed = { antennae: Point[][]; size: number };
+const isEqual = (a: Point, b: Point) => a.x === b.x && a.y === b.y;
+const gradient = (a: Point, b: Point) => (a.x - b.x) / (a.y - b.y);
+const distance = (a: Point, b: Point) =>
+  Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
 
-function parser(input: string): Parsed {
+function parser(input: string) {
   const lines = input.split("\n");
   const cells = lines.flatMap((line, y) =>
     line.split("").flatMap((char, x) => (char != "." ? [{ x, y, char }] : []))
   );
   return {
-    antennae: Object.values(groupBy(cells, "char")),
+    antennaeByType: Object.values(groupBy(cells, "char")),
     size: lines.length,
   };
 }
-
-function isEqual(a: Point, b: Point): boolean {
-  return a.x === b.x && a.y === b.y;
-}
-
-function pairwise<T>(arr: T[]): [T, T][] {
-  return arr.flatMap((a, i) => arr.slice(i + 1).map<[T, T]>((b) => [a, b]));
-}
+type Parsed = ReturnType<typeof parser>;
 
 function countAntipodes(
-  { size, antennae }: Parsed,
-  isAntipode: (antennaOne: Point, antennaTwo: Point, test: Point) => boolean
+  { size, antennaeByType }: Parsed,
+  isAntipode: (test: Point, antennaOne: Point, antennaTwo: Point) => boolean
 ): number {
-  const points = new Set<string>();
-  for (let x = 0; x < size; x++) {
-    for (let y = 0; y < size; y++) {
-      const test = { x, y };
-      for (const antennaLocations of antennae) {
-        for (const a of antennaLocations) {
-          for (const b of antennaLocations) {
-            if (isEqual(b, a)) continue;
-            if (isAntipode(a, b, test)) {
-              points.add(`${x},${y}`);
-              break;
-            }
-          }
-        }
-      }
-    }
-  }
-  return points.size;
+  const grid = range(size).flatMap((x) => range(size).map((y) => ({ x, y })));
+  return grid.filter((test: Point) =>
+    antennaeByType.some((antennae) =>
+      antennae.some((a) =>
+        antennae.some((b) => !isEqual(a, b) && isAntipode(test, a, b))
+      )
+    )
+  ).length;
 }
 
 function part1(data: Parsed): number {
   return countAntipodes(
     data,
-    (antennaOne, antennaTwo, test) =>
-      // Difference is twice the difference between the other antenna and the test point
-      antennaOne.x - test.x == 2 * (antennaTwo.x - test.x) &&
-      antennaOne.y - test.y == 2 * (antennaTwo.y - test.y)
+    (test, antennaOne, antennaTwo) =>
+      distance(antennaOne, test) == 2 * distance(antennaTwo, test) &&
+      gradient(antennaOne, test) === gradient(antennaTwo, test)
   );
 }
 
 function part2(data: Parsed): number {
   return countAntipodes(
     data,
-    (antennaOne, antennaTwo, test) =>
-      // One of the antennas is the test point
+    (test, antennaOne, antennaTwo) =>
       isEqual(antennaOne, test) ||
-      isEqual(antennaTwo, test) ||
-      // The gradient from the test point to the antennas is the same
-      (antennaOne.x - test.x) / (antennaOne.y - test.y) ==
-        (antennaTwo.x - test.x) / (antennaTwo.y - test.y)
+      gradient(antennaOne, test) === gradient(antennaTwo, test)
   );
 }
 
